a5937bc6da2b1dc950fe51b433be88e2d6f25db3
Ilia Egorychev <ilegorro@gmail.com>
2024-04-01 11:03:09 +0300

    Add 23-num-generate

---
+++ 23-num-generate/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"fmt"
+)
+
+func generate(ctx context.Context, start int) <-chan int {
+	out := make(chan int)
+	go func() {
+		defer close(out)
+		for i := start; ; i++ {
+			select {
+			case out <- i:
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+	return out
+}
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	generated := generate(ctx, 11)
+	for num := range generated {
+		fmt.Print(num, " ")
+		if num > 14 {
+			break
+		}
+	}
+	fmt.Println()
+}
